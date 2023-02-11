7e7518ce12b7b5ca2ec213c03b6c035a7d99c1cf
Jany Muong <me.roumuong@gmail.com>
2023-02-11 07:27:15 +0000

    run k8s locally via minikube

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -2,17 +2,21 @@
 
 # This tags and uploads an image to Docker Hub
 
-# Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=janymuong/mlapiscikit
 
-# Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run mlapiscikit \
+	--image=$dockerpath:latest --port=80
 
-
-# Step 3:
 # List kubernetes pods
+kubectl get pods
 
-# Step 4:
 # Forward the container port to a host
+kubectl port-forward mlapiscikit 8000:80
 
+## [this] bash script simulates these manual steps:
+# kubectl run mlapiscikit --image=janymuong/mlapiscikit:latest --port=80
+
+# kubectl get pods
+# kubectl port-forward mlapiscikit 8000:80
